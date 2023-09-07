65692e98b6a3e160e1ff7a2dd65e72d92c2c4ca3
surya <suryajoshi617@gmail.com>
2023-09-07 08:04:24 +0530

    server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ import postRoutes from './routes/posts.js'
 import { verifyToken } from "./middleware/auth.js";
 import User from './models/Users.js'
 import Post from './models/post.js'
-import {users,posts} from './data/index.js'
+import {users,posts} from './data/server.js'
 
 //*CONFIGRATIONS
 
