9bb92a2fceb03e003a82acd5c0e66b5830a1a478
surya <suryajoshi617@gmail.com>
2024-08-20 14:50:37 +0530

    login-credentials

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,12 +1,13 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { Box, Typography, useTheme, useMediaQuery, Card, CardContent } from "@mui/material";
 import Form from "./Form";
 
 const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const isTablet = useMediaQuery("(min-width: 768px)");
+  
   return (
-    <Box >
+    <Box>
       <Box
         width="100%"
         backgroundColor={theme.palette.background.alt}
@@ -14,9 +15,8 @@ const LoginPage = () => {
         textAlign="center"
         display="flex"
         justifyContent="center"
-        
       >
-        <Box  width={isNonMobileScreens ? "23.2%" : isTablet ? "31%" : "79%"} >
+        <Box width={isNonMobileScreens ? "23.2%" : isTablet ? "31%" : "79%"}>
           <Typography
             fontWeight="bold"
             fontSize="32px"
@@ -45,6 +45,17 @@ const LoginPage = () => {
           Welcome to MindPixel, the Social Media for Sociopaths!
         </Typography>
         <Form />
+        <Card sx={{ mt: "2rem" }}>
+          <CardContent>
+            <Typography variant="h6">Demo Login Credentials</Typography>
+            <Typography variant="body1" mt="1rem">
+              <strong>Username:</strong> jalabi67@gmail.com
+            </Typography>
+            <Typography variant="body1" mt="0.5rem">
+              <strong>Password:</strong> 123456
+            </Typography>
+          </CardContent>
+        </Card>
       </Box>
     </Box>
   );
